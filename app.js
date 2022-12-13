417fa341c9f3400e7e8b33e9bd330b2582d465df
vkusnayakuraga <vkusnayakuraga@gmail.com>
2022-12-13 15:48:46 +0000

    add 5 GET /api/articles/:article_id functionality and tests

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { getArticles } = require("./controllers/articles.controllers");
+const {
+  getArticles,
+  getArticleById,
+} = require("./controllers/articles.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
 
 const app = express();
@@ -7,6 +10,21 @@ const app = express();
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+// psql errors
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "bad request" });
+  } else next(err);
+});
+// custom errors
+app.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  }
+});
 
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "This page does not exist!" });
--- models/articles.models.js
+++ models/articles.models.js
@@ -12,3 +12,19 @@ exports.selectArticles = () => {
     return articles;
   });
 };
+
+exports.selectArticleById = (article_id) => {
+  const queryString = `
+  SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes
+  FROM articles
+  WHERE articles.article_id = $1;`;
+  return db.query(queryString, [article_id]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        message: "This article id does not exist!",
+      });
+    }
+    return rows[0];
+  });
+};
